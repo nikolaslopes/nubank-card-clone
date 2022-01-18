99a1fcc0824ee025988152545b04a20715c27112
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-18 08:49:41 -0400

    :dizzy:

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,45 @@
+import { useState } from 'react';
+
+import {
+  Container, InputContainer, Content, Header, Footer,
+} from './styles';
+
+import masterCardLogo from '../../assets/images/mc-logo.svg';
+import chipLogo from '../../assets/images/chip.png';
+import nubankLogo from '../../assets/images/nubank-logo.svg';
+
 export function Home() {
-  return <h1>aa</h1>;
+  const [name, setName] = useState('');
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <>
+      <InputContainer>
+        <input
+          type="text"
+          placeholder="Digite seu nome aqui"
+          maxLength={20}
+          value={name}
+          onChange={handleNameChange}
+        />
+      </InputContainer>
+      <Container>
+        <Content>
+          <Header>
+            <div className="img-box">
+              <img src={masterCardLogo} alt="mastercard-logo" />
+              <img src={chipLogo} alt="chip-logo" />
+            </div>
+          </Header>
+          <Footer>
+            <p>{name || 'Seu nome aqui'}</p>
+            <img src={nubankLogo} alt="nubank-logo" />
+          </Footer>
+        </Content>
+      </Container>
+    </>
+  );
 }
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,1 +1,101 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    x: -1000,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transisition: {
+      duration: 1,
+      type: 'spring',
+      bounce: 1,
+    },
+  },
+};
+
+export const Container = styled(motion.main).attrs(() => ({
+  variants: containerVariants,
+  initial: 'hidden',
+  animate: 'visible',
+}))`
+  background: ${({ theme }) => theme.colors.primary.main};
+  padding: 1em;
+  border-radius: 10px;
+  width: 15em;
+  height: 25em;
+  box-shadow: 5px 8px 8px #000;
+`;
+
+export const InputContainer = styled.div`
+  padding: 20px 0px 80px 0px;
+
+  input {
+    width: 358px;
+    height: 58px;
+
+    border: 2px solid #FCFCFC;
+    border-radius: 25px;
+    color: #fff;
+    padding: 0 16px;
+    background: none;
+    outline: none;
+    appearance: none;
+    transition: all 0.3s ease-in;
+    font-size: 18px;
+
+    ::placeholder {
+      color: #BCBCBC;
+    }
+
+    &:focus {
+      border-width: 4px;
+      border-color: ${({ theme }) => theme.colors.primary.main};
+    }
+
+    :hover {
+      border-color: ${({ theme }) => theme.colors.primary.light};
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const Header = styled.header`
+  height: 70%;
+
+  .img-box {
+    display: flex;
+    align-items: flex-start;
+  }
+
+  img[alt=mastercard-logo] {
+    width: 30%;
+  }
+
+  img[alt=chip-logo] {
+    width: 40%;
+    transform: rotate(90deg);
+  }
+
+`;
+
+export const Footer = styled.footer`
+  height: 30%;
+
+  p {
+    margin-left: 0.6em;
+    display: flex;
+  }
+
+  img[alt=nubank-logo] {
+    width: 35%;
+  }
+`;
