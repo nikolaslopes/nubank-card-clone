2d45356e7fb5971b54db2ef78ca2e0f3f77f57f2
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-26 09:18:16 -0400

    :fire: deploy

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
